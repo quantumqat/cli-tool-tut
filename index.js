b8f3b8ee24635c15bfe66312b2e0605b7214bf60
quantumqat <58191215+quantumqat@users.noreply.github.com>
2022-01-27 14:48:09 -0600

    finishing example game

--- index.js
+++ index.js
@@ -27,5 +27,58 @@ async function welcome(){
     `);
 }
 
-await welcome();
+async function askName(){
+    const answers = await inquirer.prompt({
+        name: 'player_name',
+        type: 'input',
+        message: 'What is your name',
+        default(){
+            return 'Player';
+        },
+    });
 
+    playerName = answers.player_name;
+}
+
+async function question1() {
+    const answers = await inquirer.prompt({
+        name: 'question_1',
+        type: 'list',
+        message: 'Javascript was created in 10 days then released on\n',
+        choices: [
+            'May 23rd, 1995',
+            'Nov 24th, 1995',
+            'Dec 4th, 1995',
+            'Dec 17th, 1996',
+        ],
+    });
+
+    return handleAnswer(answers.question_1 == 'Dec 4th, 1995');
+}
+
+async function handleAnswer(isCorrect){
+    const spinner = createSpinner('Checking answer...').start();
+    await sleep();
+
+    if (isCorrect){
+        spinner.success({text: `Nice work ${playerName}. Has anyone ever told you that you're BOSS?`});
+    } else {
+        spinner.error({text: `Dang, ${playerName}. You lose. Game over.`});
+        process.exit(1);
+    }
+}
+
+function winner() {
+    console.clear();
+    const msg = `Congratulations, ${playerName} !\n $ 1 , 0 0 0 , 0 0 0`;
+
+    figlet(msg, (err, data) => {
+        console.log(gradient.pastel.multiline(data));
+    });
+}
+
+await welcome();
+await askName();
+
+await question1();
+winner();
